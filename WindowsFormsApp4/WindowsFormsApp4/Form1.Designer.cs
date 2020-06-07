28b06c2a9a61ad67d52ff4806a518993caf7a309
NguyenCuong <honguyencuong0898@gmail.com>
2020-06-07 12:40:32 +0700

    baitap

---
+++ WindowsFormsApp4/WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.UpdateButton = new System.Windows.Forms.Button();
+            this.EditButton = new System.Windows.Forms.Button();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.StatusBox = new System.Windows.Forms.CheckBox();
+            this.CreateDate = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.DueDate = new System.Windows.Forms.DateTimePicker();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.ShowAllButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(30, 65);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(539, 133);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(30, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(295, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(331, 12);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(75, 23);
+            this.AddButton.TabIndex = 2;
+            this.AddButton.Text = "ADD";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // UpdateButton
+            // 
+            this.UpdateButton.Location = new System.Drawing.Point(412, 13);
+            this.UpdateButton.Name = "UpdateButton";
+            this.UpdateButton.Size = new System.Drawing.Size(75, 23);
+            this.UpdateButton.TabIndex = 3;
+            this.UpdateButton.Text = "UPDATE";
+            this.UpdateButton.UseVisualStyleBackColor = true;
+            this.UpdateButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // EditButton
+            // 
+            this.EditButton.Location = new System.Drawing.Point(30, 204);
+            this.EditButton.Name = "EditButton";
+            this.EditButton.Size = new System.Drawing.Size(75, 23);
+            this.EditButton.TabIndex = 4;
+            this.EditButton.Text = "EDIT";
+            this.EditButton.UseVisualStyleBackColor = true;
+            this.EditButton.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Location = new System.Drawing.Point(494, 207);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(75, 23);
+            this.DeleteButton.TabIndex = 5;
+            this.DeleteButton.Text = "DELETE";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            this.DeleteButton.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // StatusBox
+            // 
+            this.StatusBox.AutoSize = true;
+            this.StatusBox.Location = new System.Drawing.Point(254, 43);
+            this.StatusBox.Name = "StatusBox";
+            this.StatusBox.Size = new System.Drawing.Size(54, 17);
+            this.StatusBox.TabIndex = 11;
+            this.StatusBox.Text = "status";
+            this.StatusBox.UseVisualStyleBackColor = true;
+            this.StatusBox.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // CreateDate
+            // 
+            this.CreateDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.CreateDate.Location = new System.Drawing.Point(92, 38);
+            this.CreateDate.Name = "CreateDate";
+            this.CreateDate.Size = new System.Drawing.Size(125, 20);
+            this.CreateDate.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Creat_date";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(377, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Due_date";
+            // 
+            // DueDate
+            // 
+            this.DueDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.DueDate.Location = new System.Drawing.Point(437, 39);
+            this.DueDate.Name = "DueDate";
+            this.DueDate.Size = new System.Drawing.Size(132, 20);
+            this.DueDate.TabIndex = 15;
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.Location = new System.Drawing.Point(493, 12);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(75, 23);
+            this.SearchButton.TabIndex = 16;
+            this.SearchButton.Text = "SEARCH";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            this.SearchButton.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // ShowAllButton
+            // 
+            this.ShowAllButton.Location = new System.Drawing.Point(199, 207);
+            this.ShowAllButton.Name = "ShowAllButton";
+            this.ShowAllButton.Size = new System.Drawing.Size(150, 23);
+            this.ShowAllButton.TabIndex = 17;
+            this.ShowAllButton.Text = "SHOW ALL";
+            this.ShowAllButton.UseVisualStyleBackColor = true;
+            this.ShowAllButton.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(581, 242);
+            this.Controls.Add(this.ShowAllButton);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.DueDate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CreateDate);
+            this.Controls.Add(this.StatusBox);
+            this.Controls.Add(this.DeleteButton);
+            this.Controls.Add(this.EditButton);
+            this.Controls.Add(this.UpdateButton);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.Button UpdateButton;
+        private System.Windows.Forms.Button EditButton;
+        private System.Windows.Forms.Button DeleteButton;
+        private System.Windows.Forms.CheckBox StatusBox;
+        private System.Windows.Forms.DateTimePicker CreateDate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker DueDate;
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.Button ShowAllButton;
+    }
+}
+
---
+++ WindowsFormsApp4/WindowsFormsApp4/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection conn = new MySqlConnection("Server=localhost; User Id=root; Password ='';Database=db_cs1");
+        MySqlDataAdapter adapter = new MySqlDataAdapter();
+        MySqlCommand command = new MySqlCommand();
+        public DataSet ds = new DataSet();
+        public String currentid = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetRecords();
+        }
+        public void GetRecords()
+        {
+            ds = new DataSet();
+            adapter = new MySqlDataAdapter("select * from tbl_task",conn);
+            adapter.Fill(ds, "tbl_task");
+            dataGridView1.DataSource = ds;
+            dataGridView1.DataMember = "tbl_task";
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ds = new DataSet();
+            var done = 0;
+            var checkboxvalues = StatusBox.Checked;
+            if (checkboxvalues == true)
+            {
+                done = 1;
+            }
+            var query = "insert into tbl_task(`task_name`, `status`,`creat_date`,`due_date`) VALUES('" + textBox1.Text + "','" + done + "','" + CreateDate.Value + "','" + DueDate.Value + "')";
+            adapter = new MySqlDataAdapter(query,conn);
+            adapter.Fill(ds, "tbl_task");
+            textBox1.Clear();
+            GetRecords();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ds = new DataSet();
+            var done = 0;
+            var checkboxvalues = StatusBox.Checked;
+            if (checkboxvalues == true)
+            {
+                done = 1;
+            }
+            var query = "update tbl_task set `task_name`= '" + textBox1.Text + "',`status` ='" + done + "',`creat_date`='" + CreateDate.Value + "',`due_date`='" + DueDate.Value + "' where id = " + currentid;
+            adapter = new MySqlDataAdapter(query, conn);
+            adapter.Fill(ds, "tbl_task");
+            textBox1.Clear();
+            GetRecords();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int i = dataGridView1.CurrentRow.Index;
+            currentid = dataGridView1[0, i].Value.ToString();
+            textBox1.Text = dataGridView1[1, i].Value.ToString();
+            var status = Convert.ToBoolean(dataGridView1[2, i].Value);
+            StatusBox.Checked = status;
+            var creat_date = Convert.ToDateTime(dataGridView1[3, i].Value);
+            CreateDate.Value= creat_date;
+            var due_date = Convert.ToDateTime(dataGridView1[4, i].Value);
+            DueDate.Value = due_date;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int i = dataGridView1.CurrentRow.Index;
+            currentid = dataGridView1[0, i].Value.ToString();
+            ds = new DataSet();
+            adapter = new MySqlDataAdapter("delete from tbl_task where id = "+currentid,conn);
+            adapter.Fill(ds, "tbl_task");
+            textBox1.Clear();
+            GetRecords();
+        }
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void loadGrip()
+        {
+            ds = new DataSet();
+            adapter = new MySqlDataAdapter("select * from tbl_task where task_name like '" + textBox1.Text + "'", conn);
+            adapter.Fill(ds, "tbl_task");
+            dataGridView1.DataSource = ds;
+            dataGridView1.DataMember = "tbl_task";
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            loadGrip();
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            GetRecords();
+        }
+    }
+}
